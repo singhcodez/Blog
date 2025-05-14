76f4540281633faff4eb35f5c9c0b66ea9a61981
newrepo <jasnoor155@gmail.com>
2025-05-14 13:24:21 +0530

    update

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 export const books=[
         {
-        name:"The-Art-of-war",
+        name:"The-Art-of-War",
         author:"Sun Tzu",
         quotes:
         ['"There is no instance of country having benefited from prolonged warfare."',
         '"It is a matter of life and death, a road either to safety or to ruin. Hence it is a subject of inquiry which can no account be neglected."',
-        '"The supreme art of war is to subdue the enemy without fighting"."',
+        '"The supreme art of war is to subdue the enemy without fighting."',
         '"He will win who knows when to fight when not to fight."',
         '"Victorious warriors win first and then go to war, while defeated warriors go to war first and then seek to win."',
         '"If you focus everywhere, you will be weak everywhere."',
@@ -15,14 +15,25 @@ export const books=[
         '"Opportunities multiply, when you take the advantage of them."',
         '"The greatest victory is that which requires no battle."',
         '"The battle is won in the mind long before it is won on the field."',
-        '"When your army has crossed the border, you should burn your boats and bridges, in order to make it clear to everybody that you have no hankering after home."'
-        
-                
+        '"When your army has crossed the border, you should burn your boats and bridges, in order to make it clear to everybody that you have no hankering after home."',
+        '"Your can hesitations lead you to disaster. "',
+        '"Ponder and deliberate before you make a move. "',
+        '"The expert in battle move the enemy, and is not moved by them. "',
+        '"Appear weak when you are strong and weak when you are strong. All Warfare is about deception."',
+         '"Even the finest sword get rusted because of salt water."',
+         '"Be like a two headed snake.No matter from where the enemy attacks, you will counter from another angle."',
+         '"Great success can be achieved with small steps."',
+         '"if you want to fight, first count the cost of it."',
+         '"Let your plans be dark as night,and when you move fall like a thunderbolt."',
+         '"He will win who knows how to handle both superior & inferior forces."',
+         '"The wise warrior avoids the battle."',
+         '"Do not repeat the tactics which have gained you one victory, but let your methods be regulated by the infinite variety of circumstances."',
+         '"Water adjust its form according to the nature of the ground over which it flows, the soldier should also adjust his tactics in relation to the foe whom he is facing."'
         ]
         },
         {
-        name:"bro2",
-        author:"writer1",
+        name:"The-laws-of-human-nature",
+        author:"Robert Greene",
         quotes:["be calm","fresh"]
         }
         ]
