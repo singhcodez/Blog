43b655d70d7703ac02b15e4c1f45abfaabe0c8fb
newrepo <jasnoor155@gmail.com>
2025-06-10 16:26:36 +0530

    update

--- quotes/quote.js
+++ quotes/quote.js
@@ -1,1 +1,19 @@
 // integrate nav bar + next buttons using js 
+import {books} from "../../data.js";
+//console.log(books);
+const footer=``;
+const navbar=``;
+
+
+const openNav =()=> {
+            document.getElementById("myOverlay").style.height = "90%"; // Set to 50% of viewport height
+            document.getElementById("myOverlay").classList.add("open"); // Add 'open' class for rotation
+        }
+    document.getElementById("show").addEventListener("click",openNav)
+        
+const closeNav=()=> {
+            document.getElementById("myOverlay").style.height = "0%";
+            document.getElementById("myOverlay").classList.remove("open"); // Remove 'open' class to reset rotation
+        }
+
+  document.getElementById("close").addEventListener("click",closeNav)
